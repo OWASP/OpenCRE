d77db7df155e3f0d113933f4fe869b1e4631d440
Spyros <northdpole@users.noreply.github.com>
2021-08-28 14:02:20 +0100

    Rm wiki site (#1)
    remove the owasp wiki site in preparation to making cre_Sync the root proj

---
+++ scripts/cre_sync/cre_defs.py
@@ -0,0 +1,238 @@
+from pprint import pprint
+from dataclasses import dataclass
+from enum import Enum
+import json
+# used for serialising and deserialising yaml CRE documents
+
+
+class ExportFormat(Enum):
+    separator = ":"
+    section = "section"
+    subsection = "subsection"
+    hyperlink = "hyperlink"
+    link_type = "link_type"
+    name = "name"
+    id = "id"
+    description = "description"
+    cre_link = "Linked_CRE_"
+    cre = "CRE"
+
+    @staticmethod
+    def section_key(sname: str):
+        "returns <sname>:section"
+        return "%s%s%s" % (sname, ExportFormat.separator.value, ExportFormat.section.value)
+    
+    @staticmethod
+    def subsection_key(sname: str):
+        "returns <sname>:subsection"
+        return "%s%s%s" % (sname, ExportFormat.separator.value, ExportFormat.subsection.value)
+    
+    @staticmethod
+    def hyperlink_key(sname: str):
+        "returns <sname>:hyperlink"
+        return "%s%s%s" % (sname, ExportFormat.separator.value, ExportFormat.hyperlink.value)
+
+    @staticmethod
+    def link_type_key(sname: str):
+        "returns <sname>:link_type"
+        return "%s%s%s" % (sname, ExportFormat.separator.value, ExportFormat.link_type.value)
+
+    @staticmethod
+    def linked_cre_id_key(name: str):
+        "returns Linked_CRE_<name>:id"
+        return "%s%s%s%s" % (ExportFormat.cre_link.value, name, ExportFormat.separator.value, ExportFormat.id.value)
+    
+    @staticmethod
+    def linked_cre_name_key(name: str):
+        "returns Linked_CRE_<name>:name"
+        return "%s%s%s%s" % (ExportFormat.cre_link.value, name, ExportFormat.separator.value, ExportFormat.name.value)
+    
+    @staticmethod
+    def linked_cre_link_type_key(name: str):
+        "returns Linked_CRE_<name>:link_type"
+        return "%s%s%s%s" % (ExportFormat.cre_link.value, name, ExportFormat.separator.value, ExportFormat.link_type.value)
+    
+    @staticmethod
+    def cre_id_key():
+        "returns CRE:id"
+        return "%s%s%s" % (ExportFormat.cre.value, ExportFormat.separator.value, ExportFormat.id.value)
+    
+    @staticmethod
+    def cre_name_key():
+        "returns CRE:name"
+        return "%s%s%s" % (ExportFormat.cre.value, ExportFormat.separator.value, ExportFormat.name.value)
+    
+    @staticmethod
+    def cre_description_key():
+        "returns CRE:description"
+        return "%s%s%s" % (ExportFormat.cre.value, ExportFormat.separator.value, ExportFormat.description.value)
+
+
+class Credoctypes(Enum):
+    CRE = "CRE"
+    Standard = "Standard"
+
+
+class LinkTypes(Enum):
+    Same = "SAM"
+
+    @staticmethod
+    def from_str(name):
+        if name == "SAM":
+            return LinkTypes.Same
+        raise ValueError('"{}" is not a valid link type'.format(name))
+
+
+@dataclass
+class Metadata():
+    labels: {}
+
+    def __init__(self, labels={}):
+        self.labels = labels
+
+    def todict(self):
+        return self.labels
+
+
+@dataclass
+class Link():
+    ltype: LinkTypes
+    tags: list
+    document = None
+
+    def __init__(self, ltype=LinkTypes.Same, tags=[], document=None):
+        if document is None:
+            raise_MandatoryFieldException('Links need to link to a Document')
+        self.document = document
+        if type(ltype) == str:
+            self.ltype = LinkTypes.from_str(ltype)
+        else:
+            self.ltype = ltype
+        self.tags = tags
+   
+    def __hash__(self):
+        return hash(json.dumps(self.todict()))
+
+    def __eq__(self, other):
+        return self.ltype == other.ltype and \
+            self.tags == other.tags and \
+            self.document == other.document
+
+    def todict(self):
+        res = {'type': self.ltype.value}
+        if self.document:
+            res['document'] = self.document.todict()
+        if len(self.tags):
+            res['tags'] = self.tags
+        return res
+
+
+@dataclass
+class Document():
+    doctype: Credoctypes
+    id: str
+    description: str
+    name: str
+    links: list
+    tags: list
+    metadata: Metadata
+
+    def __eq__(self, other):
+        return self.id == other.id and \
+            self.name == other.name and \
+            self.doctype.value == other.doctype.value and \
+            self.description == other.description and \
+            self.links == other.links and \
+            self.tags == other.tags and \
+            self.metadata == other.metadata
+
+    def __hash__(self):
+        return hash(json.dumps(self.todict()))
+
+    def todict(self):
+        result = {
+            'doctype': self.doctype.value,
+            'name': self.name,
+        }
+        if self.description:
+            result['description'] = self.description
+        if self.id:
+            result['id'] = self.id
+        if self.links:
+            result['links'] = []
+            for link in self.links:
+                result['links'].append(link.todict())
+        if self.tags:
+            result['tags'] = self.tags
+        if self.metadata:
+            result['metadata'] = self.metadata.todict()
+        return result
+
+    def add_link(self, link: Link):
+        if not self.links:
+            self.links = []
+        if type(link).__name__ != Link.__name__:
+            raise ValueError("add_link only takes Link() types")
+
+        self.links.append(link)
+
+    def __init__(self, name, doctype=None, id="", description="", links=[], tags=[], metadata: Metadata = None):
+        self.description = str(description)
+        self.name = str(name) or raise_MandatoryFieldException(
+            "Document name not defined for document of doctype %s" % doctype)
+        self.links = links or []
+        if '' in tags:
+            tags.remove('')
+        self.tags = tags
+        self.id = id
+        self.metadata = metadata
+        if not doctype and not self.doctype:
+            raise_MandatoryFieldException("You need to set doctype")
+
+
+@dataclass
+class CRE(Document):
+    def __init__(self, *args, **kwargs):
+        self.doctype = Credoctypes.CRE
+        super().__init__(*args, **kwargs)
+    def __hash__(self):
+        return super().__hash__()
+
+@dataclass
+class Standard(Document):
+    doctype = Credoctypes.Standard
+    section: str
+    subsection: str
+    hyperlink: str
+
+    def todict(self):
+        result = super().todict()
+        result['section'] = self.section
+        result['subsection'] = self.subsection
+        result['hyperlink'] = self.hyperlink
+        return result
+
+    def __hash__(self):
+        return super().__hash__()
+    def __eq__(self, other):
+        return super().__eq__(other) and \
+            self.section == other.section and \
+            self.subsection == other.subsection and \
+            self.hyperlink == other.hyperlink
+
+    def __init__(self, section=None, subsection=None, hyperlink=None, *args, **kwargs):
+        self.doctype = Credoctypes.Standard
+        if section is None or section == '':
+            raise MandatoryFieldException("%s:%s is an invalid standard entry,"
+                                          "you can't register an entire standard at once, it needs to have sections" % (kwargs.get('name'), section))
+        self.section = section
+        self.subsection = subsection
+        self.hyperlink = hyperlink
+        super().__init__(*args, **kwargs)
+
+
+class MandatoryFieldException(Exception):
+    pass
+
+
+def raise_MandatoryFieldException(msg=''): raise MandatoryFieldException(msg)
---
+++ scripts/cre_sync/spreadsheet_utils_test.py
@@ -0,0 +1,247 @@
+import uuid
+import yaml
+import base64
+import os
+import unittest
+import cre_defs as defs
+import db
+import tempfile
+from pprint import pprint
+from spreadsheet_utils import *
+import tempfile
+
+
+class TestDB(unittest.TestCase):
+
+    def test_prepare_spreadsheet_standards(self):
+        """
+        Given:
+                * 1 CRE "CREname" that links to
+                    ** 2 subsections of Standard "ConflictStandName"
+                    ** 2 subsections in  standards "NormalStand1" and "NormalStand2"
+                    ** CRE "CREGroup"
+                * 1 CRE  "CREGroup" that links to
+                    ** CRE "CREname"
+                    ** 1 subsection in standard "GroupStand2"
+                * 1 Standard  "LoneStand"
+        Expect: an array with 5 elements
+                    * 1 element contains the mappings of  "CREname" to "NormalStand1", "NormalStand2", "CREGroup"  and 1 subsection of "ConflictStandName"
+                    * 1 element contains ONLY the mapping of "CREname" to the remaining subsection of "ConflictStandName"
+                    * 1 element contains the mappings of "CREGroup" to "CREname" and "GroupStand2"
+                    * 1 element contains the entry of "LoneStand" without any mappings
+                    * 1 element contains the entry of "OtherLoneStand" without any mappings
+        """
+        collection = db.Standard_collection(cache_file='')
+
+        dbcre = db.CRE(description="CREdesc", name="CREname",
+                       external_id="06-06-06")
+        dbgroup = db.CRE(description="CREGroupDesc",
+                         name="CREGroup", external_id="09-09-09")
+        collection.session.add(dbcre)
+        collection.session.add(dbgroup)
+        collection.session.commit()
+        collection.session.add(db.InternalLinks(
+            cre=dbcre.id, group=dbgroup.id))
+
+        conflict1 = db.Standard(subsection='4.5.1', section='ConflictStandSection',
+                                name='ConflictStandName', link="https://example.com/1")
+        conflict2 = db.Standard(subsection='4.5.2', section='ConflictStandSection',
+                                name='ConflictStandName', link="https://example.com/2")
+        collection.session.add(conflict1)
+        collection.session.add(conflict2)
+        collection.session.commit()
+        collection.session.add(db.Links(cre=dbcre.id, standard=conflict1.id))
+        collection.session.add(db.Links(cre=dbcre.id, standard=conflict2.id))
+
+        dbs1 = db.Standard(subsection='4.5.1', section='NormalStandSection1',
+                           name="NormalStand1", link='https://example.com/1')
+        dbs2 = db.Standard(subsection='4.5.2', section='NormalStandSection2',
+                           name="NormalStand2", link='https://example.com/2')
+        dbsg = db.Standard(subsection='4.5.2', section='GroupStandSection2',
+                           name="GroupStand2", link='https://example.com/g2')
+        dbls1 = db.Standard(subsection='4.5.2', section='LoneStandSection',
+                            name="LoneStand", link='https://example.com/ls1')
+        dbls2 = db.Standard(subsection='4.5.2', section='OtherLoneStandSection',
+                            name="OtherLoneStand", link='https://example.com/ls2')
+        collection.session.add(dbs1)
+        collection.session.add(dbs2)
+        collection.session.add(dbsg)
+        collection.session.add(dbls1)
+        collection.session.add(dbls2)
+        collection.session.commit()
+        collection.session.add(db.Links(cre=dbcre.id, standard=dbs1.id))
+        collection.session.add(db.Links(cre=dbcre.id, standard=dbs2.id))
+        collection.session.add(db.Links(cre=dbgroup.id, standard=dbsg.id))
+        collection.session.commit()
+
+        expected = [{'CRE:description': 'CREGroupDesc', 'CRE:id': '09-09-09', 'CRE:name': 'CREGroup',
+                     'ConflictStandName:hyperlink': None, 'ConflictStandName:link_type': None, 'ConflictStandName:section': None, 'ConflictStandName:subsection': None,
+                     'GroupStand2:hyperlink': 'https://example.com/g2', 'GroupStand2:link_type': 'SAM', 'GroupStand2:section': 'GroupStandSection2', 'GroupStand2:subsection': '4.5.2',
+                     'Linked_CRE_0:id': '06-06-06', 'Linked_CRE_0:link_type': 'SAM', 'Linked_CRE_0:name': 'CREname',
+                     'LoneStand:hyperlink': None, 'LoneStand:link_type': None, 'LoneStand:section': None, 'LoneStand:subsection': None,
+                     'NormalStand1:hyperlink': None, 'NormalStand1:link_type': None, 'NormalStand1:section': None, 'NormalStand1:subsection': None,
+                     'NormalStand2:hyperlink': None, 'NormalStand2:link_type': None, 'NormalStand2:section': None, 'NormalStand2:subsection': None,
+                     'OtherLoneStand:hyperlink': None, 'OtherLoneStand:link_type': None, 'OtherLoneStand:section': None, 'OtherLoneStand:subsection': None},
+                    {'CRE:description': 'CREdesc', 'CRE:id': '06-06-06', 'CRE:name': 'CREname',
+                     'ConflictStandName:hyperlink': 'https://example.com/1', 'ConflictStandName:link_type': 'SAM', 'ConflictStandName:section': 'ConflictStandSection', 'ConflictStandName:subsection': '4.5.1',
+                     'GroupStand2:hyperlink': None, 'GroupStand2:link_type': None, 'GroupStand2:section': None, 'GroupStand2:subsection': None,
+                     'Linked_CRE_0:id': '09-09-09', 'Linked_CRE_0:link_type': 'SAM', 'Linked_CRE_0:name': 'CREGroup',
+                     'LoneStand:hyperlink': None, 'LoneStand:link_type': None, 'LoneStand:section': None, 'LoneStand:subsection': None,
+                     'NormalStand1:hyperlink': 'https://example.com/1', 'NormalStand1:link_type': 'SAM', 'NormalStand1:section': 'NormalStandSection1', 'NormalStand1:subsection': '4.5.1',
+                     'NormalStand2:hyperlink': 'https://example.com/2', 'NormalStand2:link_type': 'SAM', 'NormalStand2:section': 'NormalStandSection2', 'NormalStand2:subsection': '4.5.2',
+                     'OtherLoneStand:hyperlink': None, 'OtherLoneStand:link_type': None, 'OtherLoneStand:section': None, 'OtherLoneStand:subsection': None},
+                    {'CRE:description': 'CREdesc', 'CRE:id': '06-06-06', 'CRE:name': 'CREname',
+                     'ConflictStandName:hyperlink': 'https://example.com/2', 'ConflictStandName:link_type': 'SAM', 'ConflictStandName:section': 'ConflictStandSection', 'ConflictStandName:subsection': '4.5.2',
+                     'GroupStand2:hyperlink': None, 'GroupStand2:link_type': None, 'GroupStand2:section': None, 'GroupStand2:subsection': None,
+                     'Linked_CRE_0:id': None, 'Linked_CRE_0:link_type': None, 'Linked_CRE_0:name': None,
+                     'LoneStand:hyperlink': None, 'LoneStand:link_type': None, 'LoneStand:section': None, 'LoneStand:subsection': None,
+                     'NormalStand1:hyperlink': None, 'NormalStand1:link_type': None, 'NormalStand1:section': None, 'NormalStand1:subsection': None,
+                     'NormalStand2:hyperlink': None, 'NormalStand2:link_type': None, 'NormalStand2:section': None, 'NormalStand2:subsection': None,
+                     'OtherLoneStand:hyperlink': None, 'OtherLoneStand:link_type': None, 'OtherLoneStand:section': None, 'OtherLoneStand:subsection': None},
+                    {'CRE:description': None, 'CRE:id': None, 'CRE:name': None,
+                     'ConflictStandName:hyperlink': None, 'ConflictStandName:link_type': None, 'ConflictStandName:section': None, 'ConflictStandName:subsection': None,
+                     'GroupStand2:hyperlink': None, 'GroupStand2:link_type': None, 'GroupStand2:section': None, 'GroupStand2:subsection': None,
+                     'Linked_CRE_0:id': None, 'Linked_CRE_0:link_type': None, 'Linked_CRE_0:name': None,
+                     'LoneStand:hyperlink': 'https://example.com/ls1', 'LoneStand:link_type': None, 'LoneStand:section': 'LoneStandSection', 'LoneStand:subsection': '4.5.2',
+                     'NormalStand1:hyperlink': None, 'NormalStand1:link_type': None, 'NormalStand1:section': None, 'NormalStand1:subsection': None,
+                     'NormalStand2:hyperlink': None, 'NormalStand2:link_type': None, 'NormalStand2:section': None, 'NormalStand2:subsection': None,
+                     'OtherLoneStand:hyperlink': None, 'OtherLoneStand:link_type': None, 'OtherLoneStand:section': None, 'OtherLoneStand:subsection': None},
+                    {'CRE:description': None, 'CRE:id': None, 'CRE:name': None,
+                     'ConflictStandName:hyperlink': None, 'ConflictStandName:link_type': None, 'ConflictStandName:section': None, 'ConflictStandName:subsection': None,
+                     'GroupStand2:hyperlink': None, 'GroupStand2:link_type': None, 'GroupStand2:section': None, 'GroupStand2:subsection': None,
+                     'Linked_CRE_0:id': None, 'Linked_CRE_0:link_type': None, 'Linked_CRE_0:name': None,
+                     'LoneStand:hyperlink': None, 'LoneStand:link_type': None, 'LoneStand:section': None, 'LoneStand:subsection': None,
+                     'NormalStand1:hyperlink': None, 'NormalStand1:link_type': None, 'NormalStand1:section': None, 'NormalStand1:subsection': None,
+                     'NormalStand2:hyperlink': None, 'NormalStand2:link_type': None, 'NormalStand2:section': None, 'NormalStand2:subsection': None,
+                     'OtherLoneStand:hyperlink': 'https://example.com/ls2', 'OtherLoneStand:link_type': None, 'OtherLoneStand:section': 'OtherLoneStandSection', 'OtherLoneStand:subsection': '4.5.2'}]
+
+        result = prepare_spreadsheet(collection, collection.export(dir=tempfile.mkdtemp()))
+        assert all([a == b for a, b in zip(result, expected)])
+
+    def test_prepare_spreadsheet_groups(self):
+        """ Given:
+                * 1 CRE "CREname" that links to
+                    ** 2 subsections of Standard "ConflictStandName"
+                    ** 2 subsections in  standards "NormalStand1" and "NormalStand2"
+                    ** CRE "CREGroup"
+                * 1 CRE  "CREGroup" that links to
+                    ** CRE "CREname"
+                    ** 1 subsection in standard "GroupStand2"
+            Expect: an array with 3 elements
+                    * 1 element contains the mappings of  "CREname" to "NormalStand1", "NormalStand2", "CREGroup"  and 1 subsection of "ConflictStandName"
+                    * 1 element contains ONLY the mapping of "CREname" to the remaining subsection of "ConflictStandName"
+                    * 1 element contains the mappings of "CREGroup" to "CREname" and "GroupStand2"
+        """
+        collection = db.Standard_collection(cache_file='')
+
+        dbcre = db.CRE(description="CREdesc", name="CREname",
+                       external_id="06-06-06")
+        dbgroup = db.CRE(description="CREGroupDesc",
+                         name="CREGroup", external_id="09-09-09")
+        collection.session.add(dbcre)
+        collection.session.add(dbgroup)
+        collection.session.commit()
+        collection.session.add(db.InternalLinks(
+            cre=dbcre.id, group=dbgroup.id))
+
+        conflict1 = db.Standard(subsection='4.5.1', section='ConflictStandSection',
+                                name='ConflictStandName', link="https://example.com/1")
+        conflict2 = db.Standard(subsection='4.5.2', section='ConflictStandSection',
+                                name='ConflictStandName', link="https://example.com/2")
+        collection.session.add(conflict1)
+        collection.session.add(conflict2)
+        collection.session.commit()
+        collection.session.add(db.Links(cre=dbcre.id, standard=conflict1.id))
+        collection.session.add(db.Links(cre=dbcre.id, standard=conflict2.id))
+
+        dbs1 = db.Standard(subsection='4.5.1', section='NormalStandSection1',
+                           name="NormalStand1", link='https://example.com/1')
+        dbs2 = db.Standard(subsection='4.5.2', section='NormalStandSection2',
+                           name="NormalStand2", link='https://example.com/2')
+        dbsg = db.Standard(subsection='4.5.2', section='GroupStandSection2',
+                           name="GroupStand2", link='https://example.com/g2')
+        collection.session.add(dbs1)
+        collection.session.add(dbs2)
+        collection.session.add(dbsg)
+        collection.session.commit()
+        collection.session.add(db.Links(cre=dbcre.id, standard=dbs1.id))
+        collection.session.add(db.Links(cre=dbcre.id, standard=dbs2.id))
+        collection.session.add(db.Links(cre=dbgroup.id, standard=dbsg.id))
+        collection.session.commit()
+
+        expected = [{'CRE:description': 'CREGroupDesc', 'CRE:id': '09-09-09', 'CRE:name': 'CREGroup',
+                     'ConflictStandName:hyperlink': None, 'ConflictStandName:link_type': None, 'ConflictStandName:section': None, 'ConflictStandName:subsection': None,
+                     'GroupStand2:hyperlink': 'https://example.com/g2', 'GroupStand2:link_type': 'SAM', 'GroupStand2:section': 'GroupStandSection2', 'GroupStand2:subsection': '4.5.2',
+                     'Linked_CRE_0:id': '06-06-06', 'Linked_CRE_0:link_type': 'SAM', 'Linked_CRE_0:name': 'CREname',
+                     'NormalStand1:hyperlink': None, 'NormalStand1:link_type': None, 'NormalStand1:section': None, 'NormalStand1:subsection': None,
+                     'NormalStand2:hyperlink': None, 'NormalStand2:link_type': None, 'NormalStand2:section': None, 'NormalStand2:subsection': None},
+                    {'CRE:description': 'CREdesc', 'CRE:id': '06-06-06', 'CRE:name': 'CREname',
+                     'ConflictStandName:hyperlink': 'https://example.com/1', 'ConflictStandName:link_type': 'SAM', 'ConflictStandName:section': 'ConflictStandSection', 'ConflictStandName:subsection': '4.5.1',
+                     'GroupStand2:hyperlink': None, 'GroupStand2:link_type': None, 'GroupStand2:section': None, 'GroupStand2:subsection': None,
+                     'Linked_CRE_0:id': '09-09-09', 'Linked_CRE_0:link_type': 'SAM', 'Linked_CRE_0:name': 'CREGroup',
+                     'NormalStand1:hyperlink': 'https://example.com/1', 'NormalStand1:link_type': 'SAM', 'NormalStand1:section': 'NormalStandSection1', 'NormalStand1:subsection': '4.5.1',
+                     'NormalStand2:hyperlink': 'https://example.com/2', 'NormalStand2:link_type': 'SAM', 'NormalStand2:section': 'NormalStandSection2', 'NormalStand2:subsection': '4.5.2'},
+                    {'CRE:description': 'CREdesc', 'CRE:id': '06-06-06', 'CRE:name': 'CREname',
+                     'ConflictStandName:hyperlink': 'https://example.com/2', 'ConflictStandName:link_type': 'SAM', 'ConflictStandName:section': 'ConflictStandSection', 'ConflictStandName:subsection': '4.5.2',
+                     'GroupStand2:hyperlink': None, 'GroupStand2:link_type': None, 'GroupStand2:section': None, 'GroupStand2:subsection': None,
+                     'Linked_CRE_0:id': None, 'Linked_CRE_0:link_type': None, 'Linked_CRE_0:name': None,
+                     'NormalStand1:hyperlink': None, 'NormalStand1:link_type': None, 'NormalStand1:section': None, 'NormalStand1:subsection': None,
+                     'NormalStand2:hyperlink': None, 'NormalStand2:link_type': None, 'NormalStand2:section': None, 'NormalStand2:subsection': None}]
+
+        result = prepare_spreadsheet(collection, collection.export(dir=tempfile.mkdtemp()))
+
+        assert all([a == b for a, b in zip(result, expected)])
+
+    def test_prepare_spreadsheet_simple(self):
+        """ Given: 
+                * 1 CRE "CREname" that links to 
+                    ** 2 subsections of Standard "ConflictStandName"
+                    ** 2 subsections in  standards "NormalStand0" and "NormalStand1"
+            Expect: an array with 2 elements
+                    * 1 element contains the mappings of  "CREname" to "NormalStand1", "NormalStand0" and 1 subsection of "ConflictStandName"
+                    * 1 element contains ONLY the mapping of "CREname" to the remaining subsection of "ConflictStandName"
+        """
+        # empty string means temporary db
+        collection = db.Standard_collection(cache_file="")
+
+        # test 0, single CRE, connects to several standards, 1 cre maps to the same standard in multiple sections/subsections
+        dbcre = db.CRE(description="CREdesc", name="CREname",
+                       external_id='123-321-0')
+        collection.session.add(dbcre)
+
+        conflict0 = db.Standard(subsection='4.5.0', section="ConflictStandSection",
+                                name="ConflictStandName", link='https://example.com/0')
+        conflict1 = db.Standard(subsection='4.5.1', section="ConflictStandSection",
+                                name="ConflictStandName", link='https://example.com/1')
+        collection.session.add(conflict0)
+        collection.session.add(conflict1)
+        collection.session.commit()
+        collection.session.add(db.Links(cre=dbcre.id, standard=conflict0.id))
+        collection.session.add(db.Links(cre=dbcre.id, standard=conflict1.id))
+
+        dbs0 = db.Standard(subsection='4.5.0', section='NormalStandSection0',
+                           name='NormalStand0', link='https://example.com/0')
+        dbs1 = db.Standard(subsection='4.5.1', section='NormalStandSection1',
+                           name='NormalStand1', link='https://example.com/1')
+        collection.session.add(dbs0)
+        collection.session.add(dbs1)
+        collection.session.commit()
+        collection.session.add(db.Links(cre=dbcre.id, standard=dbs0.id))
+        collection.session.add(db.Links(cre=dbcre.id, standard=dbs1.id))
+
+        expected = [{'CRE:name': 'CREname', 'CRE:id': '123-321-0', 'CRE:description': 'CREdesc',
+                     'ConflictStandName:hyperlink': 'https://example.com/0',  'ConflictStandName:link_type': 'SAM', 'ConflictStandName:section': 'ConflictStandSection', 'ConflictStandName:subsection': '4.5.0',
+                     'NormalStand0:hyperlink': 'https://example.com/0', 'NormalStand0:link_type': 'SAM', 'NormalStand0:section': 'NormalStandSection0', 'NormalStand0:subsection': '4.5.0',
+                     'NormalStand1:hyperlink': 'https://example.com/1', 'NormalStand1:link_type': 'SAM', 'NormalStand1:section': 'NormalStandSection1', 'NormalStand1:subsection': '4.5.1'},
+                    {'CRE:name': 'CREname', 'CRE:id': '123-321-0', 'CRE:description': 'CREdesc',
+                     'ConflictStandName:hyperlink': 'https://example.com/1', 'ConflictStandName:link_type': 'SAM', 'ConflictStandName:section': 'ConflictStandSection', 'ConflictStandName:subsection': '4.5.1',
+                     'NormalStand0:hyperlink': None, 'NormalStand0:link_type': None, 'NormalStand0:section': None, 'NormalStand0:subsection': None,
+                     'NormalStand1:hyperlink': None, 'NormalStand1:link_type': None, 'NormalStand1:section': None, 'NormalStand1:subsection': None}]
+
+        result = prepare_spreadsheet(collection, collection.export(dir=tempfile.mkdtemp()))
+
+        assert all([a == b for a, b in zip(result, expected)])
+
+
+if __name__ == '__main__':
+    unittest.main()
