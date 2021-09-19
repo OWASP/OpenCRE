8e005343669c74b699c06ea87c47626800b371e2
Meess <abcmaster8@hotmail.com>
2021-09-19 15:06:37 +0200

    ADD generic groupby functionality

--- application/frontend/src/utils/document.ts
+++ application/frontend/src/utils/document.ts
@@ -14,3 +14,11 @@ export const getLinksByType = (node: Document): LinksByType =>
         return acc;
       }, {})
     : {};
+
+export const groupBy = <T, K extends keyof any>(list: T[], getKey: (item: T) => K) =>
+  list.reduce((previous, currentItem) => {
+    const group = getKey(currentItem);
+    if (!previous[group]) previous[group] = [];
+    previous[group].push(currentItem);
+    return previous;
+}, {} as Record<K, T[]>);
