d9552943837f3e66b478986d9e322cb70b8e9c68
Sanda Golcea <sanda.golcea@gmail.com>
2021-09-23 00:06:01 +0100

    Fixes for mobile view (#31)

--- application/frontend/src/App.tsx
+++ application/frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import './app.scss';
 
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
@@ -9,19 +9,16 @@ import { MainContentArea } from './scaffolding';
 
 const queryClient = new QueryClient();
 
-interface IAppProps {}
-
-const App: FunctionComponent<IAppProps> = () => {
-  return (
-    <div className="app">
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Toaster />
-          <MainContentArea />
-        </BrowserRouter>
-      </QueryClientProvider>
-    </div>
-  );
-};
+const App = () => (
+  <div className="app">
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Toaster />
+        <MainContentArea />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </div>
+);
 
 export default App;
+
