338e679952a34547387ee3bb4977226de224b1d6
northdpole <morfeas3000@gmail.com>
2022-01-29 20:24:42 +0000

    linting

--- application/utils/external_project_parsers/zap_alerts_parser.py
+++ application/utils/external_project_parsers/zap_alerts_parser.py
@@ -7,10 +7,21 @@ from application.defs import cre_defs as defs
 import os
 import re
 
-def zap_alert(name:str,id:str,description:str,tags:List[str],code:str)->defs.Tool:
-    return defs.Tool(tooltype=defs.ToolTypes.Offensive, name=f"ZAP Alert: {name}",id=id,description=description,tags=tags,hyperlink=code)
 
-def parse_zap_alerts(cache:db.Node_collection):
+def zap_alert(
+    name: str, id: str, description: str, tags: List[str], code: str
+) -> defs.Tool:
+    return defs.Tool(
+        tooltype=defs.ToolTypes.Offensive,
+        name=f"ZAP Alert: {name}",
+        id=id,
+        description=description,
+        tags=tags,
+        hyperlink=code,
+    )
+
+
+def parse_zap_alerts(cache: db.Node_collection):
     zaproxy_website = "https://github.com/zaproxy/zaproxy-website.git"
     alerts_path = "site/content/docs/alerts/"
     zap_md_cwe_regexp = r"cwe: ?(?P<cweId>\d+)"
@@ -21,44 +32,49 @@ def parse_zap_alerts(cache:db.Node_collection):
     zap_md_code_regexp = r"code: ?(?P<code>\".+\")"
 
     repo = git.clone(zaproxy_website)
-    for mdfile in os.listdir(os.path.join(repo.working_dir,alerts_path)):
-        pth = os.path.join(repo.working_dir,alerts_path,mdfile)
+    for mdfile in os.listdir(os.path.join(repo.working_dir, alerts_path)):
+        pth = os.path.join(repo.working_dir, alerts_path, mdfile)
         name = None
         externalId = None
         tag = None
         description = None
-        code=None
+        code = None
         with open(pth) as mdf:
             mdtext = mdf.read()
-            title = re.search(zap_md_title_regexp,mdtext)
+            title = re.search(zap_md_title_regexp, mdtext)
             if title:
-                name = title.group('title')
+                name = title.group("title")
 
-            id = re.search(zap_md_alert_id_regexp,mdtext)
+            id = re.search(zap_md_alert_id_regexp, mdtext)
             if id:
-                externalId = id.group('id')
-            
-            type_tag = re.search(zap_md_alert_type_regexp,mdtext)
+                externalId = id.group("id")
+
+            type_tag = re.search(zap_md_alert_type_regexp, mdtext)
             if type_tag:
-                tag = type_tag.group('type')
-            
-            desc = re.search(zap_md_solution_regexp,mdtext)
+                tag = type_tag.group("type")
+
+            desc = re.search(zap_md_solution_regexp, mdtext)
             if desc:
-                description = desc.group('solution')
-            cd = re.search(zap_md_code_regexp,mdtext)
+                description = desc.group("solution")
+            cd = re.search(zap_md_code_regexp, mdtext)
             if cd:
-                code = desc.group('code')
+                code = desc.group("code")
 
             cwe = re.search(zap_md_cwe_regexp, mdtext)
             if cwe:
-                cweId = cwe.group('cweId')
-                cwe_nodes = cache.get_nodes(name="CWE",section=cweId)
+                cweId = cwe.group("cweId")
+                cwe_nodes = cache.get_nodes(name="CWE", section=cweId)
                 for node in cwe_nodes:
                     for link in node.links:
                         if link.document.doctype == defs.Credoctypes.CRE:
-                            alert = zap_alert(name=name,id=externalId,description=description,tags=[tag],code=code)
+                            alert = zap_alert(
+                                name=name,
+                                id=externalId,
+                                description=description,
+                                tags=[tag],
+                                code=code,
+                            )
                             dbnode = cache.add_node(alert)
-                            cache.add_link(cre=db.dbCREfromCRE(link.document),node=dbnode)
-
-
-
+                            cache.add_link(
+                                cre=db.dbCREfromCRE(link.document), node=dbnode
+                            )
