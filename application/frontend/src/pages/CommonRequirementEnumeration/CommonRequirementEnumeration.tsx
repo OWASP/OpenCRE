89a81bf99eb6d2b54148fa50d53e1742309b6dc6
flowirtz <6052785+flowirtz@users.noreply.github.com>
2022-02-25 10:22:50 +0000

    💄 Display export button on CRE page

--- application/frontend/src/pages/CommonRequirementEnumeration/CommonRequirementEnumeration.tsx
+++ application/frontend/src/pages/CommonRequirementEnumeration/CommonRequirementEnumeration.tsx
@@ -1,27 +1,28 @@
 import './commonRequirementEnumeration.scss';
 
-import React, { useEffect, useMemo, useState, useContext } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 
 import { DocumentNode } from '../../components/DocumentNode';
+import ExportButton from '../../components/ExportButton/export-button';
+import { ClearFilterButton, FilterButton } from '../../components/FilterButton/FilterButton';
 import { LoadingAndErrorIndicator } from '../../components/LoadingAndErrorIndicator';
 import { DOCUMENT_TYPE_NAMES } from '../../const';
 import { useEnvironment } from '../../hooks';
+import { applyFilters } from '../../hooks/applyFilters';
 import { Document } from '../../types';
 import { groupLinksByType } from '../../utils';
-import { applyFilters, filterContext } from '../../hooks/applyFilters';
-import { ClearFilterButton, FilterButton } from '../../components/FilterButton/FilterButton';
 
 export const CommonRequirementEnumeration = () => {
   const { id } = useParams();
   const { apiUrl } = useEnvironment();
   const [loading, setLoading] = useState<boolean>(false);
-  const globalState = useContext(filterContext)
 
-  const { error, data, refetch } = useQuery<{ data: Document; }, string>(
+  const FETCH_URL = `${apiUrl}/id/${id}`;
+  const { error, data, refetch } = useQuery<{ data: Document }, string>(
     'cre',
-    () => fetch(`${apiUrl}/id/${id}`).then((res) => res.json()),
+    () => fetch(FETCH_URL).then((res) => res.json()),
     {
       retry: false,
       enabled: false,
@@ -38,14 +39,14 @@ export const CommonRequirementEnumeration = () => {
   }, [id]);
 
   const cre = data?.data;
-  let filteredCRE
-  if(cre != undefined){
-    filteredCRE = applyFilters(JSON.parse(JSON.stringify(cre))) // dirty deepcopy
+  let filteredCRE;
+  if (cre != undefined) {
+    filteredCRE = applyFilters(JSON.parse(JSON.stringify(cre))); // dirty deepcopy
   }
   let currentUrlParams = new URLSearchParams(window.location.search);
-  let display:Document
-  display = currentUrlParams.get("applyFilters") === "true"? filteredCRE:cre 
-  
+  let display: Document;
+  display = currentUrlParams.get('applyFilters') === 'true' ? filteredCRE : cre;
+
   const linksByType = useMemo(() => (display ? groupLinksByType(display) : {}), [display]);
 
   return (
@@ -53,37 +54,54 @@ export const CommonRequirementEnumeration = () => {
       <LoadingAndErrorIndicator loading={loading} error={error} />
       {!loading && !error && display && (
         <>
-          <h4 className="cre-page__heading">{display.name}</h4>
+          <h4 className="cre-page__heading">
+            {display.name}
+            <ExportButton fetchURL={FETCH_URL} />
+          </h4>
           <h5 className="cre-page__sub-heading">{display.id}</h5>
           <div className="cre-page__description">{display.description}</div>
-          { display && display.hyperlink &&
+          {display && display.hyperlink && (
             <>
               <span>Reference: </span>
-              <a href={display?.hyperlink} target="_blank"> { display.hyperlink }</a>
+              <a href={display?.hyperlink} target="_blank">
+                {' '}
+                {display.hyperlink}
+              </a>
             </>
-          }
-          {display.tags?
-          <div className="cre-page__tags">Tags:{display.tags.map((tag) => ( <b>{tag} </b>))}</div>:""}
+          )}
+          {display.tags ? (
+            <div className="cre-page__tags">
+              Tags:{' '}
+              {display.tags.map((tag) => (
+                <b key={tag}>{tag} </b>
+              ))}
+            </div>
+          ) : (
+            ''
+          )}
 
-          {currentUrlParams.get("applyFilters")==="true"?
-          <div className="cre-page__filters">  
-          Filtering on:
-            {currentUrlParams.getAll("filters").map((filter)=>(
-              <b key={filter}>{filter.replace("s:","").replace("c:","")}, </b>))}
-              
-          <ClearFilterButton/>
-          </div>:""}
+          {currentUrlParams.get('applyFilters') === 'true' ? (
+            <div className="cre-page__filters">
+              Filtering on:
+              {currentUrlParams.getAll('filters').map((filter) => (
+                <b key={filter}>{filter.replace('s:', '').replace('c:', '')}, </b>
+              ))}
+              <ClearFilterButton />
+            </div>
+          ) : (
+            ''
+          )}
           <div className="cre-page__links-container">
             {Object.keys(linksByType).length > 0 &&
               Object.entries(linksByType).map(([type, links]) => (
                 <div className="cre-page__links" key={type}>
                   <div className="cre-page__links-eader">
-                  {display.doctype}:{display.id}: {display.name} <b>{DOCUMENT_TYPE_NAMES[type]}</b>:
+                    {display.doctype}:{display.id}: {display.name} <b>{DOCUMENT_TYPE_NAMES[type]}</b>:
                   </div>
                   {links.map((link, i) => (
                     <div key={i} className="accordion ui fluid styled cre-page__links-container">
                       <DocumentNode node={link.document} linkType={type} />
-                      <FilterButton document={link.document}/>
+                      <FilterButton document={link.document} />
                     </div>
                   ))}
                 </div>
