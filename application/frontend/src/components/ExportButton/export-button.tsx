9a600b57dc80aa239ad14bcdcc5ccbc450e1bdd6
flowirtz <6052785+flowirtz@users.noreply.github.com>
2022-02-25 11:20:46 +0000

    ✨ Add query param support to ExportButton

--- application/frontend/src/components/ExportButton/export-button.tsx
+++ application/frontend/src/components/ExportButton/export-button.tsx
@@ -5,20 +5,44 @@ import { Loader } from 'semantic-ui-react';
 
 interface IExportButton {
   fetchURL: string;
+  fetchParams?: string[][];
 }
 
-export const openURLInNewTab = (url: string): void => {
+const openURLInNewTab = (url: string): void => {
   const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
   if (newWindow) newWindow.opener = null;
 };
 
-const ExportButton = ({ fetchURL }: IExportButton) => {
+/**
+ * Returns the export URL for a given API endpoint.
+ * Handles the CRE, search and standard endpoints.
+ *
+ * Also handles query parameters as part of the `url` or as part of `params`.
+ * @param url original fetch URL
+ * @param params (optional) parameters that were passed to Axios
+ * @returns computed request url to get an export of the endpoint
+ */
+const getExportURL = (url: string, params?: string[][]): string => {
+  const EXPORT_STRING = '/export';
+  if (url.includes('?')) {
+    const [prefix, queryParams] = url.split('?');
+    return prefix + EXPORT_STRING + '?' + queryParams;
+  }
+
+  if (params) {
+    return url + '/export?' + new URLSearchParams(params['params']).toString();
+  }
+
+  return url + EXPORT_STRING;
+};
+
+const ExportButton = ({ fetchURL, fetchParams }: IExportButton) => {
   const [isLoading, setLoading] = useState(false);
 
   const fetchSpreadsheetURLAndOpen = () => {
     setLoading(true);
 
-    fetch(fetchURL + '/export')
+    fetch(getExportURL(fetchURL, fetchParams))
       .then((response) => response.json())
       .then((data) => {
         if (!data || !data.status || data.status !== 'ok') {
