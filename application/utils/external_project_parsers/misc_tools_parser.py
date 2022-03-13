8a6734e1f81d71ea76b39c5e3f77b03a8acceaab
northdpole <morfeas3000@gmail.com>
2022-03-13 17:47:04 +0000

    misc parsers works

--- application/utils/external_project_parsers/misc_tools_parser.py
+++ application/utils/external_project_parsers/misc_tools_parser.py
@@ -1,6 +1,8 @@
 # script to parse CRE links from  README.md files of a given list of projects
 import os
 import re
+import urllib
+from pprint import pprint
 from typing import List
 
 from application.database import db
@@ -8,10 +10,15 @@ from application.defs import cre_defs as defs
 from application.utils import git
 
 tool_urls = [
-    "https://github.com/commjoen/wrongsecrets"
+    "https://github.com/commjoen/wrongsecrets",
+    # "https://github.com/northdpole/wrongsecrets.git",
 
 ]
 def Project(name: str, hyperlink: str, tags: List[str],ttype:str,description:str) -> defs.Tool:
+    pprint(name)
+    print(ttype)
+    print(" ".join(tags))
+    print(description)
     return defs.Tool(
         name=name,
         tooltype=defs.ToolTypes.from_str(ttype),
@@ -22,33 +29,50 @@ def Project(name: str, hyperlink: str, tags: List[str],ttype:str,description:str
 
 
 def parse_tool(tool_repo:str, cache: db.Node_collection):
-    # TODO: need to figure a way to provide all the necessary info in one hyperlink, perhaps osib can be leveraged for this
-    
+
     repo = git.clone(tool_repo)
-    readme = os.listdir(os.path.join(repo.working_dir, "README.md"))
+    readme = os.path.join(repo.working_dir, "README.md")
+    title_regexp = r"# (?P<title>(\w+ )+)"
+    cre_link = r".*\[.*\]\((?P<url>(https\:\/\/www\.)?opencre\.org\/cre\/(?P<cre>\d+-\d+).*)"
+
     with open(readme) as rdf:
         mdtext = rdf.read()
+
         if "opencre.org" not in mdtext:
+            pprint("didn't find a link, bye")
             return
         title = re.search(title_regexp, mdtext)
-        cre = re.search(cre_link, mdtext)
+        cre = re.search(cre_link, mdtext,flags=re.IGNORECASE)
 
         if cre and title:
-            name = title.group("title")
-            cre_id = cre.group("cre")
-            cres = cache.get_CREs(external_id=cre_id)
-            hyperlink = f"{tool_repo.replace('.git','')}/{readme}"
-            for dbcre in cres:
-                cs = Project(
-                    name=name,
-                    hyperlink=hyperlink,
-                    tags=[],
-                    ttype=ttype,
-                    description=description,
-                )
-                dbnode = cache.add_node(cs)
-                cache.add_link(
-                    cre=db.dbCREfromCRE(dbcre),
-                    node=dbnode,
-                    type=defs.LinkTypes.LinkedTo,
-                )
+            parsed = urllib.parse.urlparse(cre.group("url"))
+            values = urllib.parse.parse_qs(parsed.query)
+            
+            name = title.group("title").strip()
+            cre_id = cre.group("cre").strip()
+            register = True if 'register' in values else False
+            type = values.get("type")[0] or "Tool" # this parser matches tools so this is really optional
+            tool_type =  values.get("tool_type")[0] or "Unknown"
+            description = values.get("description")[0] or ""
+            tags = values.get("tags")[0].split(",") if "tags" in values else []
+            if cre_id and register:
+                cres = cache.get_CREs(external_id=cre_id)
+                hyperlink = (
+                        f"{tool_repo.replace('.git','')}"
+                    )
+                for dbcre in cres:
+                    cs = Project(
+                        name=name,
+                        hyperlink=hyperlink,
+                        tags=tags or [],
+                        ttype=tool_type,
+                        description=description,
+                    )
+                    pprint(cs)
+                    pprint(cres)
+                    # dbnode = cache.add_node(cs)
+                    # cache.add_link(
+                    #     cre=db.dbCREfromCRE(dbcre),
+                    #     node=dbnode,
+                    #     type=defs.LinkTypes.LinkedTo,
+                    # )
