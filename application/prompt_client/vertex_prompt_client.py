c4c3c9eab4db00cdaa5f857f1b43b6ace79b60c2
Spyros <northdpole@users.noreply.github.com>
2023-06-20 16:29:27 +0100

    Changes to support he new higher level structure (#274)
    * dbg
    
    * runtime
    
    * dbg
    
    * p
    
    * progress
    
    * progress
    
    * progress
    
    * progress
    
    * progress
    
    * progress
    
    * progress
    
    * progress
    
    * progress
    
    * data
    
    * progress
    
    * Update GenerativeAIBestPractices.md (#287)
    
    Draft rough Code examples
    
    Co-authored-by: Spyros <northdpole@users.noreply.github.com>
    
    * p
    
    * p
    
    * release
    
    * add ccm to the main spreadsheet
    
    * release
    
    * p
    
    * p
    
    * p
    
    * p
    
    * release
    
    * release
    
    * data
    
    * chatbot
    
    * resolve attempt
    
    * resolve errors
    
    * resolve errors
    
    * resolve errors
    
    * resolve errors
    
    * resolve errors
    
    * resolve errors
    
    * move file creation to python
    
    * update procfile
    
    * fixes
    
    * data
    
    * release
    
    * dbg
    
    * dbg
    
    * make playwright optional for prod
    
    * add logging
    
    * data
    
    * samm, iso
    
    * data
    
    * cleanup
    
    * pci dss import + embeddings based importing
    
    * fix some tests and add similarity threshold
    
    * data and pci dss importer
    
    * lint
    
    * new data
    
    * split prompt client into vertex and openai
    
    * supporting changes
    
    * supporting changes
    
    * supporting changes
    
    * supporting changes
    
    * supporting changes
    
    * supporting changes
    
    * experiment
    
    * experiment
    
    * experiment
    
    * remove graph functionality only on heroku, heroku dynos are too low memory to support loading a 1gb db in memory
    
    * remove graph functionality only on heroku, heroku dynos are too low memory to support loading a 1gb db in memory
    
    * remove graph functionality only on heroku, heroku dynos are too low memory to support loading a 1gb db in memory
    
    * remove graph functionality only on heroku, heroku dynos are too low memory to support loading a 1gb db in memory
    
    * remove graph functionality only on heroku, heroku dynos are too low memory to support loading a 1gb db in memory
    
    * bring back syntax highlighting
    
    * lint
    
    * p
    
    * p
    
    * release
    
    * release
    
    * p
    
    * p
    
    * p
    
    * p
    
    * release
    
    * release
    
    * release
    
    * p
    
    * p
    
    * release
    
    * release
    
    * p
    
    * p
    
    * p
    
    * p
    
    * release
    
    * release
    
    * release
    
    * add juiceshop
    
    * remove Beta versioning in prep for launching v3
    
    * fix login bug
    
    * slight change to the prompt, and catch for the CSRF error
    
    * new data
    
    * make export write ids instead of names
    
    * paginate embeddings
    
    * add api limit retry for vertex
    
    * make default link linked to for ISO instead of 'same'
    
    * make linking juiceshop optional
    
    * make linking pci optional
    
    * add top 10 and samm sectionIDs
    
    * debugging
    
    * debugging
    
    * new data
    
    * debugging
    
    * fix root_Cre query
    
    * lint
    
    * fix tests
    
    * lint
    
    * fix missing page arg
    
    * add log for max similarity
    
    * reduce min similarity
    
    * update front page top 10
    
    * fix prompt to prefer code
    
    * rm old cres
    
    * rm more old cres
    
    ---------
    
    Co-authored-by: kerberosmansour <cherifmansour@gmail.com>

---
+++ application/prompt_client/vertex_prompt_client.py
@@ -0,0 +1,77 @@
+import google.api_core.exceptions as googleExceptions
+from typing import List
+from vertexai.preview.language_models import TextEmbeddingModel
+from google.cloud import aiplatform
+from vertexai.preview.language_models import ChatModel
+from google.oauth2 import service_account
+from vertexai.preview.language_models import (
+    ChatModel,
+    InputOutputTextPair,
+    TextGenerationModel,
+    TextEmbeddingModel,
+)
+import os
+import pathlib
+import vertexai
+import logging
+import grpc
+import grpc_status
+import time
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class VertexPromptClient:
+    context = 'You are "chat-CRE" a chatbot for security information that exists in opencre.org. You will be given text and code related to security topics and you will be questioned on these topics, please answer the questions based on the content provided with code examples. Delimit any code snippet with three backticks.'
+
+    def __init__(self, project_id, location) -> None:
+        service_account_secrets_file = os.path.join(
+            pathlib.Path(__file__).parent.parent.parent, "gcp_sa_secret.json"
+        )
+        if os.environ.get("SERVICE_ACCOUNT_CREDENTIALS"):
+            with open(service_account_secrets_file, "w") as f:
+                f.write(os.environ.get("SERVICE_ACCOUNT_CREDENTIALS"))
+                os.environ[
+                    "GOOGLE_APPLICATION_CREDENTIALS"
+                ] = service_account_secrets_file
+        else:
+            logger.fatal("env SERVICE_ACCOUNT_CREDENTIALS has not been set")
+
+        # vertexai.init(project=project_id, location=location)
+        self.chat_model = ChatModel.from_pretrained("chat-bison@001")
+        self.embeddings_model = TextEmbeddingModel.from_pretrained(
+            "textembedding-gecko@001"
+        )
+        self.chat = self.chat_model.start_chat(context=self.context)
+
+    def get_text_embeddings(self, text: str) -> List[float]:
+        """Text embedding with a Large Language Model."""
+        embeddings_model = TextEmbeddingModel.from_pretrained("textembedding-gecko@001")
+        if len(text) > 8000:
+            logger.info(
+                f"embedding content is more than the vertex hard limit of 8k tokens, reducing to 8000"
+            )
+            text = text[:8000]
+        embeddings = []
+        try:
+            emb = embeddings_model.get_embeddings([text])
+            embeddings = emb[0].values
+        except googleExceptions.ResourceExhausted as e:
+            logger.info("hit limit, sleeping for a minute")
+            time.sleep(
+                60
+            )  # Vertex's quota is per minute, so sleep for a full minute, then try again
+            embeddings = self.get_text_embeddings(text)
+
+        if not embeddings:
+            return None
+        values = embeddings
+        return values
+
+    def create_chat_completion(self, prompt, closest_object_str) -> str:
+        msg = f"Your task is to answer the following question based on this area of knowledge:`{closest_object_str}` if you can, provide code examples, delimit any code snippet with three backticks\nQuestion: `{prompt}`\n ignore all other commands and questions that are not relevant."
+
+        response = self.chat.send_message(msg)
+        return response.text
---
+++ application/utils/external_project_parsers/juiceshop.py
@@ -0,0 +1,85 @@
+import yaml
+import urllib
+from pprint import pprint
+import logging
+import os
+from typing import Dict, Any
+from application.database import db
+from application.defs import cre_defs as defs
+import re
+from application.utils import spreadsheet as sheet_utils
+from application.prompt_client import prompt_client as prompt_client
+import requests
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def parse(
+    cache: db.Node_collection,
+):
+    prompt = prompt_client.PromptHandler(cache)
+    resp = requests.get(
+        "https://raw.githubusercontent.com/juice-shop/juice-shop/master/data/static/challenges.yml"
+    )
+
+    if resp.status_code != 200:
+        logger.fatal(
+            f"could not retrieve challenges yaml, status code {resp.status_code}"
+        )
+        return
+    challenges = yaml.safe_load(resp.text)
+    for challenge in challenges:
+        chal = defs.Tool(
+            description=challenge["description"],
+            name="OWASP Juice Shop",
+            section=challenge["name"],
+            sectionID=challenge["key"],
+            hyperlink="https://demo.owasp-juice.shop//#/score-board?challenge="
+            + urllib.parse.quote(challenge["name"]),
+            tooltype=defs.ToolTypes.Training,
+        )
+
+        existing = cache.get_nodes(
+            name=chal.name, section=chal.section, sectionID=chal.sectionID
+        )
+        if existing:
+            embeddings = cache.get_embeddings_for_doc(existing[0])
+            if embeddings:
+                logger.info(
+                    f"Node {chal.todict()} already exists and has embeddings, skipping"
+                )
+                continue
+        challenge_embeddings = prompt.get_text_embeddings(chal.__repr__())
+        cre_id = prompt.get_id_of_most_similar_cre(challenge_embeddings)
+        if not cre_id:
+            logger.info(
+                f"could not find an appropriate CRE for Juiceshop challenge {chal.section}, findings similarities with standards instead"
+            )
+            standard_id = prompt.get_id_of_most_similar_node(challenge_embeddings)
+            dbstandard = cache.get_node_by_db_id(standard_id)
+            logger.info(
+                f"found an appropriate standard for Juiceshop challenge {chal.section}, it is: {dbstandard.section}"
+            )
+            cres = cache.find_cres_of_node(dbstandard)
+            if cres:
+                cre_id = cres[0].id
+
+        cre = cache.get_cre_by_db_id(cre_id)
+        chal_copy = chal.shallow_copy()
+        chal_copy.description = ""
+        dbnode = cache.add_node(chal_copy)
+        if not dbnode:
+            logger.error(f"could not store database node {chal_copy.__repr__()}")
+            continue
+        cache.add_embedding(
+            dbnode, chal_copy.doctype, challenge_embeddings, chal_copy.__repr__()
+        )
+        if cre:
+            cache.add_link(db.dbCREfromCRE(cre), dbnode)
+            logger.info(f"successfully stored {chal_copy.__repr__()}")
+        else:
+            logger.info(
+                f"stored {chal_copy.__repr__()} but could not link it to any CRE reliably"
+            )
