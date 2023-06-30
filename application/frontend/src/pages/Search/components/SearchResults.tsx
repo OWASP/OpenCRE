6b690a95b97fd5b0d8dde045041e54187c3affce
john681611 <john681611@hotmail.com>
2023-06-30 21:03:49 +0100

    Initial attempt at separating documents in search results

--- application/frontend/src/pages/Search/components/SearchResults.tsx
+++ application/frontend/src/pages/Search/components/SearchResults.tsx
@@ -5,12 +5,17 @@ import { getDocumentDisplayName } from 'application/frontend/src/utils/document'
 
 export const SearchResults = ({ results }) => {
   if (results && results.length != 0) {
+    const sortedResults = results.sort((a, b) => getDocumentDisplayName(a).localeCompare(getDocumentDisplayName(b)))
+    let lastDocumentName = sortedResults[0].id ?? sortedResults[0].name
     return (
       <>
-        {results.sort((a, b) => getDocumentDisplayName(a).localeCompare(getDocumentDisplayName(b))).map((document, i) => (
-          <div key={i} className="accordion ui fluid styled standard-page__links-container">
-            <DocumentNode node={document} linkType={'Standard'} />
-          </div>
+        {sortedResults.map((document, i) => (
+          <>
+            {lastDocumentName !== (document.id ?? document.name) &&<hr style={{marginTop: "20px"}} />}
+            <div key={i} className="accordion ui fluid styled standard-page__links-container">
+              <DocumentNode node={document} linkType={'Standard'} />
+            </div>
+          </>
         ))}
       </>
     );
