6feb7459c7df3e132d73ed3cdeff032a7a6e35aa
john681611 <john681611@hotmail.com>
2023-07-10 18:47:29 +0100

    Improved seperation in nested node

--- application/frontend/src/pages/Search/components/SearchResults.tsx
+++ application/frontend/src/pages/Search/components/SearchResults.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 import { DocumentNode } from '../../../components/DocumentNode';
 import { getDocumentDisplayName } from 'application/frontend/src/utils/document';
+import { DOCUMENT_TYPES } from 'application/frontend/src/const';
 
-export const SearchResults = ({ results, grouped=false }) => {
+export const SearchResults = ({ results }) => {
   if (results && results.length != 0) {
     const sortedResults = results.sort((a, b) => getDocumentDisplayName(a).localeCompare(getDocumentDisplayName(b)))
-    let lastDocumentName = sortedResults[0].id ?? sortedResults[0].name
+    let lastDocumentName = sortedResults[0].name
     return (
       <>
         {sortedResults.map((document, i) => {let temp = (
           <>
-            {grouped && lastDocumentName !== (document.id ?? document.name) &&<hr style={{marginBottom: "40px"}} />}
+            {document.doctype != DOCUMENT_TYPES.TYPE_CRE && lastDocumentName !== document.name &&<hr style={{marginBottom: "40px"}} />}
             <div key={i} className="accordion ui fluid styled standard-page__links-container">
               <DocumentNode node={document} linkType={'Standard'} />
             </div>
