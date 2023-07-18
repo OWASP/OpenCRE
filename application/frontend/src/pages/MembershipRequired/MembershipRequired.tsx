e56ffe2ca950c98c82cc9ae20a786b0edbda2888
John Harvey <10814889+john681611@users.noreply.github.com>
2023-07-18 13:42:22 +0100

    Added: Membership required page with OWASP link (#327)
    Co-authored-by: Spyros <northdpole@users.noreply.github.com>

---
+++ application/frontend/src/pages/MembershipRequired/MembershipRequired.tsx
@@ -0,0 +1,19 @@
+import './MembershipRequired.scss';
+
+import React from 'react';
+import { Button, Header } from 'semantic-ui-react';
+
+
+export const MembershipRequired = () => {
+  return (
+    <div className="membership-required">
+      <Header as="h1" className="membership-required__heading">
+        OWASP Membership Required
+      </Header>
+      <p>A OWASP Membership account is needed to login</p>
+      <Button primary href="https://owasp.org/membership/">
+          Sign up 
+      </Button>
+    </div>
+  );
+};
