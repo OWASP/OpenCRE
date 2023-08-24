1be0235437fd1275e23542672bc921f7d98e624a
john681611 <john681611@hotmail.com>
2023-08-24 10:13:09 +0100

    scoring passing tests

--- application/tests/gap_analysis_test.py
+++ application/tests/gap_analysis_test.py
@@ -4,7 +4,7 @@ from application.utils.gap_analysis import (
     get_path_score,
     get_relation_direction,
     get_next_id,
-    PENALTIES
+    PENALTIES,
 )
 
 
@@ -31,7 +31,7 @@ class TestGapAnalysis(unittest.TestCase):
         step = {"start": {"id": "123"}, "end": {"id": "234"}}
         self.assertEqual(get_next_id(step, "123"), "234")
 
-    def test_get_path_score_direct_siblings_zero(self):
+    def test_get_path_score_direct_siblings_returns_zero(self):
         path = {
             "start": {
                 "id": "029f7cd7-ef2f-4f25-b0d2-3227cde4b34b",
@@ -62,7 +62,7 @@ class TestGapAnalysis(unittest.TestCase):
         }
         self.assertEqual(get_path_score(path), 0)
 
-    def test_get_path_score_one_up_zero(self):
+    def test_get_path_score_one_up_returns_one_up_penaltiy(self):
         path = {
             "start": {
                 "id": "029f7cd7-ef2f-4f25-b0d2-3227cde4b34b",
@@ -100,4 +100,147 @@ class TestGapAnalysis(unittest.TestCase):
                 },
             ],
         }
-        self.assertEqual(get_path_score(path), PENALTIES['CONTAINS_UP'])
+        self.assertEqual(get_path_score(path), PENALTIES["CONTAINS_UP"])
+
+    def test_get_path_score_one_down_one_returns_one_down_penaltiy(self):
+        path = {
+            "start": {
+                "id": "029f7cd7-ef2f-4f25-b0d2-3227cde4b34b",
+            },
+            "end": {
+                "id": "7d030730-14cc-4c43-8927-f2d0f5fbcf5d",
+            },
+            "path": [
+                {
+                    "end": {
+                        "id": "029f7cd7-ef2f-4f25-b0d2-3227cde4b34b",
+                    },
+                    "relationship": "LINKED_TO",
+                    "start": {
+                        "id": "07bc9f6f-5387-4dc6-b277-0022ed76049f",
+                    },
+                },
+                {
+                    "end": {
+                        "id": "07bc9f6f-5387-4dc6-b277-0022ed76049f",
+                    },
+                    "relationship": "CONTAINS",
+                    "start": {
+                        "id": "123",
+                    },
+                },
+                {
+                    "end": {
+                        "id": "7d030730-14cc-4c43-8927-f2d0f5fbcf5d",
+                    },
+                    "relationship": "LINKED_TO",
+                    "start": {
+                        "id": "123",
+                    },
+                },
+            ],
+        }
+        self.assertEqual(get_path_score(path), PENALTIES["CONTAINS_DOWN"])
+
+    def test_get_path_score_related_returns_related_penalty(self):
+        path = {
+            "start": {
+                "id": "029f7cd7-ef2f-4f25-b0d2-3227cde4b34b",
+            },
+            "end": {
+                "id": "7d030730-14cc-4c43-8927-f2d0f5fbcf5d",
+            },
+            "path": [
+                {
+                    "end": {
+                        "id": "029f7cd7-ef2f-4f25-b0d2-3227cde4b34b",
+                    },
+                    "relationship": "LINKED_TO",
+                    "start": {
+                        "id": "07bc9f6f-5387-4dc6-b277-0022ed76049f",
+                    },
+                },
+                {
+                    "end": {
+                        "id": "07bc9f6f-5387-4dc6-b277-0022ed76049f",
+                    },
+                    "relationship": "RELATED",
+                    "start": {
+                        "id": "123",
+                    },
+                },
+                {
+                    "end": {
+                        "id": "7d030730-14cc-4c43-8927-f2d0f5fbcf5d",
+                    },
+                    "relationship": "LINKED_TO",
+                    "start": {
+                        "id": "123",
+                    },
+                },
+            ],
+        }
+        self.assertEqual(get_path_score(path), PENALTIES["RELATED"])
+
+    def test_get_path_score_one_of_each_returns_penalty(self):
+        path = {
+            "start": {
+                "id": "029f7cd7-ef2f-4f25-b0d2-3227cde4b34b",
+            },
+            "end": {
+                "id": "7d030730-14cc-4c43-8927-f2d0f5fbcf5d",
+            },
+            "path": [
+                {
+                    "end": {
+                        "id": "029f7cd7-ef2f-4f25-b0d2-3227cde4b34b",
+                    },
+                    "relationship": "LINKED_TO",
+                    "start": {
+                        "id": "07bc9f6f-5387-4dc6-b277-0022ed76049f",
+                    },
+                },
+                {
+                    "end": {
+                        "id": "07bc9f6f-5387-4dc6-b277-0022ed76049f",
+                    },
+                    "relationship": "CONTAINS",
+                    "start": {
+                        "id": "123",
+                    },
+                },
+                {
+                    "end": {
+                        "id": "456",
+                    },
+                    "relationship": "RELATED",
+                    "start": {
+                        "id": "123",
+                    },
+                },
+                {
+                    "end": {
+                        "id": "7d030730-14cc-4c43-8927-f2d0f5fbcf5d",
+                    },
+                    "relationship": "CONTAINS",
+                    "start": {
+                        "id": "456",
+                    },
+                },
+                {
+                    "end": {
+                        "id": "7d030730-14cc-4c43-8927-f2d0f5fbcf5d",
+                    },
+                    "relationship": "LINKED_TO",
+                    "start": {
+                        "id": "456",
+                    },
+                },
+            ],
+        }
+        self.assertEqual(
+            get_path_score(path),
+            PENALTIES["RELATED"]
+            + PENALTIES["CONTAINS_UP"]
+            + PENALTIES["CONTAINS_DOWN"],
+        )
