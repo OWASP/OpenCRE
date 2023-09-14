c09101d511d857f3366f12e2c8ccce1b749c3c33
john681611 <john681611@hotmail.com>
2023-09-14 12:45:49 +0100

    Use Standard display format and create GA type

--- application/frontend/src/types.ts
+++ application/frontend/src/types.ts
@@ -41,3 +41,19 @@ export interface PaginatedResponse {
   standards: Document[];
   total_pages: number;
 }
+
+interface GapAnalysisPathSegment {
+  start: Document;
+  end: Document;
+  relationship: string;
+};
+
+interface GapAnalysisPath {
+  end: Document;
+  path: GapAnalysisPathSegment[]
+};
+
+export interface GapAnalysisPathStart {
+  start: Document;
+  paths: Record<string, GapAnalysisPath>;
+};
