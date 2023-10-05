a1c86e226d9bd8090f42484ddef5e6b709b62412
john681611 <john681611@hotmail.com>
2023-10-05 10:23:32 +0100

    Rename share link

--- application/frontend/src/pages/GapAnalysis/GapAnalysis.tsx
+++ application/frontend/src/pages/GapAnalysis/GapAnalysis.tsx
@@ -217,7 +217,7 @@ export const GapAnalysis = () => {
                     }}
                     target="_blank"
                   >
-                    <Icon name="share square" /> Share this anyalysis
+                    <Icon name="share square" /> Copy link to analysis
                   </Button>
                 </div>
               )}
