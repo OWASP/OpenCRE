8111c40b9397a4310a8da790ea1e6d2ba705eb68
Spyros <northdpole@users.noreply.github.com>
2023-10-24 19:36:28 +0100

    use db to cache gap analysis results instead of redis (#435)
    * use db to cache gap analysis results instead of redis
    
    * lint
    
    * typo
    
    * fix 1 test, fix bugs, make cache key into its own function
    
    * lint
    
    * fix-mock
    
    * migration
    
    * Fix: bad ref
    
    * fix tests
    
    * lint
    
    * minor changes
    
    * lint
    
    ---------
    
    Co-authored-by: john681611 <john681611@hotmail.com>

--- application/utils/redis.py
+++ application/utils/redis.py
@@ -5,13 +5,14 @@ from urllib.parse import urlparse
 
 def connect():
     redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
-
-    url = urlparse(redis_url)
-    r = redis.Redis(
-        host=url.hostname,
-        port=url.port,
-        password=url.password,
-        ssl=True,
-        ssl_cert_reqs=None,
-    )
-    return r
+    if redis_url == "redis://localhost:6379":
+        return redis.from_url(redis_url)
+    else:
+        url = urlparse(redis_url)
+        return redis.Redis(
+            host=url.hostname,
+            port=url.port,
+            password=url.password,
+            ssl=True,
+            ssl_cert_reqs=None,
+        )
