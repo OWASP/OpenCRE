80310801f3fedd7008c59b55b940a206289a26d4
Spyros <morfeas3000@gmail.com>
2023-10-29 00:47:56 +0100

    fix-lint

--- application/web/web_main.py
+++ application/web/web_main.py
@@ -215,6 +215,7 @@ def find_document_by_tag() -> Any:
     logger.info("tags aborting 404")
     abort(404, "Tag does not exist")
 
+
 @app.route("/rest/v1/map_analysis", methods=["GET"])
 @cache.cached(timeout=50, query_string=True)
 def gap_analysis() -> Any:
