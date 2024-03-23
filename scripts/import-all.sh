ceef9289c1e4b90fa7110c03f3c65e9f27193c67
Spyros <morfeas3000@gmail.com>
2024-03-23 18:17:54 +0000

    fix scripts, make import-projects use scripts

--- scripts/import-all.sh
+++ scripts/import-all.sh
@@ -7,16 +7,20 @@ export GOOGLE_PROJECT_ID='opencre-vertex'
 export NEO4J_URL='neo4j://neo4j:password@127.0.0.1:7687'
 export FLASK_APP=$(pwd)/cre.py
 
-if -n $(docker ps | grep cre-neo4); then
+if [ -n $(docker ps | grep cre-neo4) ]; then
     docker stop cre-neo4j
     make docker-neo4j-rm || true
 fi
 
-if -n $(docker ps | grep cre-redis-stack); then
+if [ -n $(docker ps | grep cre-redis-stack) ]; then
     docker stop cre-redis-stack
 fi
 
-rm -rf standards_cache.sqlite
+if [ -n $CRE_DELETE_DB ]; then
+    echo "CRE_DELETE_DB is set, emptying database"
+    rm -rf standards_cache.sqlite
+fi
+
 make migrate-upgrade
 make docker-redis
 make docker-neo4j
@@ -26,8 +30,15 @@ for i in seq 1 $RUN_COUNT; do
  done
 
 [ -d "./venv" ] && . ./venv/bin/activate
-python cre.py --add --from_spreadsheet https://docs.google.com/spreadsheets/d/1eZOEYgts7d_-Dr-1oAbogPfzBLh6511b58pX3b59kvg
-python cre.py --import_external_projects
+
+if [ -z $CRE_SKIP_IMPORT_CORE ]; then
+    echo "CRE_SKIP_IMPORT_CORE is not set, importing core csv"
+    python cre.py --add --from_spreadsheet https://docs.google.com/spreadsheets/d/1eZOEYgts7d_-Dr-1oAbogPfzBLh6511b58pX3b59kvg
+fi
+if [ -z $CRE_SKIP_IMPORT_PROJECTS ]; then
+    echo "CRE_SKIP_IMPORT_PROJECTS is not set, importing external projects"
+    python cre.py --import_external_projects
+fi
 
 killall python
 killall make
