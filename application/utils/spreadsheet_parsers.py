025fcb2ca46a507d66a742342ba0d48e631a81ba
Spyros <morfeas3000@gmail.com>
2024-08-11 12:23:03 +0100

    add support for importing from new format

--- application/utils/spreadsheet_parsers.py
+++ application/utils/spreadsheet_parsers.py
@@ -135,16 +135,17 @@ def is_empty(value: Optional[str]) -> bool:
     )
 
 
-def parse_export_format(lfile: List[Dict[str, Any]]) -> Dict[str, defs.Document]:
+def parse_export_format(lfile: List[Dict[str, Any]]) -> Dict[str, List[defs.Document]]:
     """
     Given: a spreadsheet written by prepare_spreadsheet()
     return a list of CRE docs
     """
     cres: Dict[str, defs.CRE] = {}
-    standards: Dict[str, defs.Standard] = {}
+    standards: Dict[str, Dict[str, defs.Standard]] = {}
+    documents: Dict[str, List[defs.Document]] = {}
 
     if not lfile:
-        return cres.values(), standards.values()
+        return documents
 
     max_internal_cre_links = len(
         set([k for k in lfile[0].keys() if k.startswith("CRE")])
@@ -152,6 +153,8 @@ def parse_export_format(lfile: List[Dict[str, Any]]) -> Dict[str, defs.Document]
     standard_names = set(
         [k.split("|")[0] for k in lfile[0].keys() if not k.startswith("CRE")]
     )
+    logger.info(f"Found standards with names: {standard_names}")
+
     highest_cre = None
     highest_index = max_internal_cre_links + 1
 
@@ -161,7 +164,7 @@ def parse_export_format(lfile: List[Dict[str, Any]]) -> Dict[str, defs.Document]
     for mapping_line in lfile:
         working_cre = None
         working_standard = None
-        # get highest numbered CRE entry
+        # get highest numbered CRE entry (lowest in hierarchy)
         for i in range(max_internal_cre_links - 1, -1, -1):
             if not is_empty(mapping_line.get(f"CRE {i}")):
                 entry = mapping_line.get(f"CRE {i}").split(defs.ExportFormat.separator)
@@ -171,6 +174,8 @@ def parse_export_format(lfile: List[Dict[str, Any]]) -> Dict[str, defs.Document]
                         f"mapping line contents: {line}, key: CRE {i} is not formatted correctly, missing separator {defs.ExportFormat.separator}"
                     )
                 working_cre = defs.CRE(name=entry[1], id=entry[0])
+                if cres.get(working_cre.id):
+                    working_cre = cres[working_cre.id]
 
                 if previous_index < i:  # we found a higher hierarchy CRE
                     previous_index = i
@@ -182,12 +187,17 @@ def parse_export_format(lfile: List[Dict[str, Any]]) -> Dict[str, defs.Document]
                         )
                     )
                 elif highest_index < i:  # we found a higher hierarchy CRE
-                    cres[highest_cre.id] = highest_cre.add_link(
-                        defs.Link(
-                            document=working_cre.shallow_copy(),
-                            ltype=defs.LinkTypes.Contains,
+                    if not highest_cre.link_exists(working_cre):
+                        cres[highest_cre.id] = highest_cre.add_link(
+                            defs.Link(
+                                document=working_cre.shallow_copy(),
+                                ltype=defs.LinkTypes.Contains,
+                            )
                         )
-                    )
+                    else:
+                        logger.warning(
+                            f"Link between {highest_cre.name} and {working_cre.name} already exists"
+                        )
                 elif highest_cre == None:
                     highest_cre = working_cre
                     highest_index = i
@@ -198,7 +208,6 @@ def parse_export_format(lfile: List[Dict[str, Any]]) -> Dict[str, defs.Document]
 
         for s in standard_names:
             if not is_empty(mapping_line.get(f"{s}{defs.ExportFormat.separator}name")):
-
                 working_standard = defs.Standard(
                     name=s,
                     sectionID=mapping_line.get(f"{s}{defs.ExportFormat.separator}id"),
@@ -210,8 +219,12 @@ def parse_export_format(lfile: List[Dict[str, Any]]) -> Dict[str, defs.Document]
                         f"{s}{defs.ExportFormat.separator}description", ""
                     ),
                 )
-                if standards.get(working_standard.id):
-                    working_standard = standards[working_standard.id]
+                if standards.get(working_standard.name) and standards.get(
+                    working_standard.name
+                ).get(working_standard.id):
+                    working_standard = standards[working_standard.name][
+                        working_standard.id
+                    ]
 
                 if working_cre:
                     working_cre.add_link(
@@ -226,10 +239,20 @@ def parse_export_format(lfile: List[Dict[str, Any]]) -> Dict[str, defs.Document]
                             ltype=defs.LinkTypes.LinkedTo,
                         )
                     )
-                standards[working_standard.id] = working_standard
+
+                if working_standard.name not in standards:
+                    standards[working_standard.name] = {}
+
+                standards[working_standard.name][working_standard.id] = working_standard
+
         if working_cre:
             cres[working_cre.id] = working_cre
-    return cres.values(), standards.values()
+    documents[defs.Credoctypes.CRE] = list(cres.values())
+
+    for standard_name, standard_entries in standards.items():
+        logger.info(f"Adding {len(standard_entries)} entries for {standard_name}")
+        documents[standard_name] = list(standard_entries.values())
+    return documents
 
 
 @dataclass
