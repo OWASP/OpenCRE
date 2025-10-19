a09e2f3cf9f94699267998534c36b1f4e07eb817
Spyros <northdpole@users.noreply.github.com>
2025-10-19 20:14:08 +0100

    Update Map Analysis link to use relative URL
    Signed-off-by: Spyros <northdpole@users.noreply.github.com>

--- application/frontend/src/pages/Search/Search.tsx
+++ application/frontend/src/pages/Search/Search.tsx
@@ -280,7 +280,7 @@ export const SearchPage = () => {
               </div>
               <h3 className="feature-block__title">MAP ANALYSIS</h3>
               <p className="feature-block__text">
-                Utilize <strong><a href="https://www.opencre.org/map_analysis">Map Analysis</a></strong> as a tool to explore and understand the connections
+                Utilize <strong><a href="/map_analysis">Map Analysis</a></strong> as a tool to explore and understand the connections
                 between two standards.
               </p>
               <p className="feature-block__text">
