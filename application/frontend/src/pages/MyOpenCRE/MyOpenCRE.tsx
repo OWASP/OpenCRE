5f191504d40c5df703d73d90095164593c7baf78
PRAteek-singHWY <prateek23022004@gmail.com>
2025-12-31 18:10:41 +0530

    style(myopencre): move container spacing to SCSS

--- application/frontend/src/pages/MyOpenCRE/MyOpenCRE.tsx
+++ application/frontend/src/pages/MyOpenCRE/MyOpenCRE.tsx
@@ -153,7 +153,7 @@ export const MyOpenCRE = () => {
   /* ------------------ UI ------------------ */
 
   return (
-    <Container style={{ marginTop: '3rem' }}>
+    <Container className="myopencre-container">
       <Header as="h1">MyOpenCRE</Header>
 
       <p>
