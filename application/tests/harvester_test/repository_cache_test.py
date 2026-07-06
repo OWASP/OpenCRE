a0e820282b90fabd8cc7aaaa64332d8e76a12af0
ParthAggarwal16 <parth22012006@gmail.com>
2026-07-06 17:17:21 +0530

    test(harvester): align week 2 tests with project conventions

---
+++ application/tests/harvester_test/repository_cache_test.py
@@ -0,0 +1,37 @@
+import unittest
+
+from application.utils.harvester.repository_cache import (
+    build_repository_cache_path,
+)
+
+
+class RepositoryCacheTests(unittest.TestCase):
+    def test_build_repository_cache_path(self):
+        path = build_repository_cache_path(
+            "OWASP",
+            "ASVS",
+        )
+
+        self.assertEqual(
+            str(path),
+            ".harvester_cache/owasp/asvs/main",
+        )
+
+    def test_different_branches_have_different_cache_paths(self):
+        main_path = build_repository_cache_path(
+            owner="OWASP",
+            repository="ASVS",
+            branch="main",
+        )
+
+        dev_path = build_repository_cache_path(
+            owner="OWASP",
+            repository="ASVS",
+            branch="dev",
+        )
+
+        self.assertNotEqual(main_path, dev_path)
+
+
+if __name__ == "__main__":
+    unittest.main()
