1a8526a1d8f076a47fd0ba25eac6e0388dd57304
ParthAggarwal16 <parth22012006@gmail.com>
2026-07-25 19:17:14 +0530

    test(harvester): match checkout invocation

--- application/tests/harvester_test/git_repository_client_test.py
+++ application/tests/harvester_test/git_repository_client_test.py
@@ -114,6 +114,7 @@ class GitRepositoryClientTests(unittest.TestCase):
                 "-C",
                 str(client.get_local_path()),
                 "checkout",
+                "--",
                 "main",
             ],
             check=True,
