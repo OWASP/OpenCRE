0affe866494d5ffb00c583c3bf7e2b9bdb1c0f74
bornunique911 <ravirajmehta911@gmail.com>
2026-08-07 18:12:21 +0530

    fix: update OWASP Kubernetes Top Ten parser to 2025 version

--- application/cmd/cre_main.py
+++ application/cmd/cre_main.py
@@ -976,13 +976,13 @@ def run(args: argparse.Namespace) -> None:  # pragma: no cover
         BaseParser().register_resource(
             secure_headers.SecureHeaders, db_connection_str=args.cache_file
         )
-    if args.owasp_kubernetes_top10_2022_in:
+    if args.owasp_kubernetes_top10_2025_in:
         from application.utils.external_project_parsers.parsers import (
-            owasp_kubernetes_top10_2022,
+            owasp_kubernetes_top10_2025,
         )
 
         BaseParser().register_resource(
-            owasp_kubernetes_top10_2022.OwaspKubernetesTop10_2022,
+            owasp_kubernetes_top10_2025.OwaspKubernetesTop10_2025,
             db_connection_str=args.cache_file,
         )
     if args.owasp_kubernetes_top10_2025_in:
