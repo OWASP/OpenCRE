ad27d819cb73db8a533ea21e1127c8814bee0fa6
Spyros <morfeas3000@gmail.com>
2026-09-13 12:49:55 +0100

    Rework OWASP mapping importers to load shared eval fixtures.
    Keep the #953/#960 JSON as test gold behind mapping_fixtures, and stop treating those parsers as production catalog sources.

--- application/cmd/cre_main.py
+++ application/cmd/cre_main.py
@@ -25,10 +25,6 @@ from application.utils import redis
 from application.utils import db_backend
 from application.utils import gap_analysis
 from application.utils import cres_csv_export
-from application.utils.external_project_parsers.parsers import (
-    owasp_kubernetes_top10_2022,
-    owasp_kubernetes_top10_2025,
-)
 
 if TYPE_CHECKING:
     from application.prompt_client import prompt_client as prompt_client
